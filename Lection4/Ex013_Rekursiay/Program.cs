51a6fd2b3c32b15b4ef513346e78bf12cc8519f6
DimK <kochergin.d.a@yandex.ru>
2023-01-19 16:56:01 +0300

    Добавил задачу 013 с рекурсивным методом факториала

---
+++ Lection4/Ex013_Rekursiay/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double Factorial (int n) // используем тип double так как факториал 17 уже не показывает нормальный результат. Так как при типе int мы получим отрицательные числа.
+
+{
+    // 1! = 1
+    //0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+for(int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
